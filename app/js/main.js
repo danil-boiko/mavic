28f778c1ffea58fdd0f1fad6136d41e01b9d8a0c
danil-boiko <boiko.81@list.ru>
2020-10-22 19:06:10 +0300

    questions section id ready

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,12 @@ $(function () {
 
     });
 
+    $('.questions__item-title').on('click', function () {
+
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+
+    });
+
 
 });
